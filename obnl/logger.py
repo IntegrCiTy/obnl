5eba8c9f317ffa922cee261086b95554ce0bcef9
Gillian Basso <gillian.basso@epfl.ch>
2017-10-09 15:17:11 +0200

    only one logger per type

--- obnl/logger.py
+++ obnl/logger.py
@@ -5,16 +5,19 @@ from obnl.impl import node as _node
 
 
 def activate_console_logging(log_level=logging.INFO):
-    console_handler = logging.StreamHandler()
-    console_handler.setLevel(log_level)
-    console_handler.setFormatter(logging.Formatter(_node.Node.DEFAULT_LOGGING_FORMAT))
-    _node.Node.LOGGER.setLevel(1)  # TODO log all data
-    _node.Node.LOGGER.addHandler(console_handler)
+    if not hasattr(activate_console_logging, "handler"):
+        activate_console_logging.handler = logging.StreamHandler()
+        _add_handler(activate_console_logging.handler, log_level)
 
 
 def activate_file_logging(filename, log_level=logging.INFO):
-    file_handler = logging.handlers.TimedRotatingFileHandler(filename)
-    file_handler.setLevel(log_level)
-    file_handler.setFormatter(logging.Formatter(_node.Node.DEFAULT_LOGGING_FORMAT))
+    if not hasattr(activate_file_logging, "handler"):
+        activate_file_logging.handler = logging.handlers.TimedRotatingFileHandler(filename)
+        _add_handler(activate_file_logging.handler, log_level)
+
+
+def _add_handler(handler, log_level):
+    handler.setLevel(log_level)
+    handler.setFormatter(logging.Formatter(_node.Node.DEFAULT_LOGGING_FORMAT))
     _node.Node.LOGGER.setLevel(1)  # TODO log all data
-    _node.Node.LOGGER.addHandler(file_handler)
+    _node.Node.LOGGER.addHandler(handler)
